a4ad9f5ac68959d5d642dfb7471f5cdd692fad7f
Atanas Kisyov <kisyov92@gmail.com>
2023-07-28 15:34:12 +0200

    Added smooth scrolling and sticky navigation

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/////////
+// sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-header");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+/////////
+// smooth scroll
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
